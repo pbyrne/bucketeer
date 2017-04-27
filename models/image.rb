4746b3dac7fb2bb6d6aca882bdba285b7ded6f39
Patrick Byrne <code@patrickbyrne.net>
2017-04-26 20:39:38 -0500

    Better linking to images with complex filenames

--- models/image.rb
+++ models/image.rb
@@ -10,4 +10,9 @@ class Image
   def initialize(path)
     @path = Pathname.new(path).expand_path
   end
+
+  # Escape URI-invalid characters in filenames
+  def uri
+    URI.encode_www_form_component basename
+  end
 end
