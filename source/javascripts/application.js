4ddc78540794b462fedb7d490059ec97cc31473b
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2020-02-21 12:28:52 -0600

    Use vanilla JS instead of Shoestring
    Because:
    
    * I know enough JS now to do it
    * It makes the page a tad faster
    
    This commit:
    
    * Removes shoestring
    * Moves all the JS into one file (because for some reason Sprockets
      doesn't seem to be doing that with `//= require _bucketeer`
    * Moves the Bucketeer object to a modern JS class
    * Updates to use modern JS like arrow functions and const

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,8 +1,54 @@
-//= require vendor/_shoestring-1_0_2.js
-//= require _bucketeer.js
+window.performOnLoad = (callback) => {
+  if (document.readyState === 'loading') {  // Loading hasn't finished yet
+    document.addEventListener('DOMContentLoaded', callback);
+  } else {  // `DOMContentLoaded` has already fired
+    callback();
+  }
+}
 
-(function () {
-  "use strict";
-  $("html.isnt-js").removeClass("isnt-js").addClass("is-js")
-  var $bucketeer = new Bucketeer($(".m-actions form"), $(".m-images .image"))
-})();
+performOnLoad(() => {
+  const htmlElement = document.querySelector("html.isnt-js")
+
+  if (htmlElement) {
+    htmlElement.classList.remove("isnt-js")
+    htmlElement.classList.add("is-js")
+  }
+
+  window.$bucketeer = new Bucketeer({
+    form: ".m-actions form",
+    images: ".m-images .image",
+  })
+})
+
+class Bucketeer {
+  constructor(options) {
+    this.form = document.querySelector(options.form)
+    this.images = document.querySelectorAll(options.images)
+    this.bindEventListeners(this)
+  }
+
+  bindEventListeners(self) {
+    self.form.querySelector("input[data-action=filter]").addEventListener("input", self.triggerFilter.bind(self))
+  }
+
+  // "asdf" to /a.*s.*d.*f/
+  fuzzyRegExp(filterString) {
+    const pieces = filterString.split("")
+    return new RegExp(pieces.join(".*"), "i")
+  }
+
+  triggerFilter(event) {
+    const input = event.target || event.srcElement
+    const fuzzyFinder = this.fuzzyRegExp(input.value)
+    this.images.forEach((image) => {
+      var imageName = image.dataset.imageName
+
+      if (imageName.match(fuzzyFinder)) {
+        image.classList.remove("is-hidden")
+      } else {
+        image.classList.add("is-hidden")
+      }
+    })
+  }
+}
+
